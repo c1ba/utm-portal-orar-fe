648244d7b32e22eb4768f5dd32f1fbed02ee2fad
Alexandra Costache <costachealexandra28@gmail.com>
2023-02-11 22:17:48 +0200

    Updatare panou logare pentru BE

--- src/utils/apollo/client.ts
+++ src/utils/apollo/client.ts
@@ -1,10 +1,21 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-
+import {setContext} from "@apollo/client/link/context";
 const httpLink = new HttpLink({
 	uri: "http://localhost:5000/graphql"
 });
 
+const authLink = setContext(async (__, {headers}) => {
+	const token = sessionStorage.getItem("token");
+	return {
+		headers: {
+			...headers,
+			Authorization: token ? `Bearer ${token}` : "",
+			"Test123": "Test456"
+		}
+	};
+});
+
 export const apolloClient = new ApolloClient({
 	cache: new InMemoryCache(),
-	link: httpLink
+	link: authLink.concat(httpLink)
 });
